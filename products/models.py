87a7eef240951856f803aecd18b7f6eff9de74a6
mich1991 <wozniak.michal91@gmail.com>
2023-04-04 17:28:18 +0100

    Fix default image

--- products/models.py
+++ products/models.py
@@ -27,7 +27,7 @@ class Category(models.Model):
 class Product(models.Model):
 	title = models.CharField(max_length=254, unique=True)
 	short_description = models.CharField(max_length=200)
-	long_description = models.TextField()
+	long_description = models.TextField(null=True, blank=True)
 	sku = models.CharField(max_length=254, null=True, blank=True)
 	image = models.ImageField(null=True, blank=True)
 	image_url = models.URLField(max_length=1024, null=True, blank=True)
