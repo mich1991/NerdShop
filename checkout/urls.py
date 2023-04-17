b2a3bd75ed762838aa41e199cab42a448b1d710e
mich1991 <wozniak.michal91@gmail.com>
2023-04-17 19:21:05 +0100

    Add missing webhook route

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import CheckoutViewPage, CheckoutSuccessView, CacheCheckoutDataView
-
+from webhooks import webhook
 urlpatterns = [
 	path('', CheckoutViewPage.as_view(), name='checkout'),
 	path('checkout_success/<order_number>', CheckoutSuccessView.as_view(), name='checkout_success'),
 	path('cache_checkout_data/', CacheCheckoutDataView.as_view(), name='cache_checkout_data'),
+	path('wh/', webhook, name="webhook")
 ]
