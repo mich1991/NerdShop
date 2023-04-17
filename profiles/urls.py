2f0f9c99abd7af4ca7667c4a9fa4016bcd90ca38
mich1991 <wozniak.michal91@gmail.com>
2023-04-17 23:16:54 +0100

    Add Order history page

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import ProfilePageView, OrdersPageView
+from .views import ProfilePageView, OrdersPageView, OrderDetailsPageView
 
 urlpatterns = [
     path('', ProfilePageView.as_view(), name='profiles'),
-    path('orders', OrdersPageView.as_view(), name='user_orders')
+    path('orders', OrdersPageView.as_view(), name='user_orders'),
+    path('orders/<int:pk>', OrderDetailsPageView.as_view(), name='user_order_details')
+
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect, HttpResponseRedirect, reverse
-from django.views.generic import View
+from django.views.generic import View, ListView, DetailView
+
+from checkout.models import Order
 from .models import Profile
 from .forms import ProfileForm
+
+
 # Create your views here.
 
 
@@ -29,4 +33,15 @@ class ProfilePageView(View):
 
 class OrdersPageView(View):
 	def get(self, request):
-		return render(request, 'profiles/orders.html')
+		profile = get_object_or_404(Profile, user=request.user)
+		orders = profile.orders.all().order_by('-date')
+		ctx = {
+			'orders': orders,
+		}
+
+		return render(request, 'profiles/orders.html', ctx)
+
+
+class OrderDetailsPageView(DetailView):
+	model = Order
+	template_name = 'profiles/order_details.html'
