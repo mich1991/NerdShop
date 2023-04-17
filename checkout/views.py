0e1e65b42eb00a818578a9c987ef6dad3e1b3549
mich1991 <wozniak.michal91@gmail.com>
2023-04-17 18:07:32 +0100

    Add checkout views & checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.views.generic import View
 from products.models import Product
+from profiles.forms import ProfileForm
 from .forms import OrderForm
-from .models import OrderLineItem
+from .models import OrderLineItem, Order
 from django.contrib import messages
 from cart.context import cart_contents
 from profiles.models import Profile
 from django.conf import settings
-# Create your views here.
 import json
 import stripe
 
@@ -22,11 +22,11 @@ class CheckoutViewPage(View):
 
 		if not cart:
 			messages.error(request,
-			               "There's nothing in your bag at the moment")
+			               "There's nothing in your cart at the moment")
 			return redirect(reverse('product_list'))
 
-		current_bag = cart_contents(request)
-		total = current_bag['grand_total']
+		current_cart = cart_contents(request)
+		total = current_cart['grand_total']
 		stripe_total = round(total * 100)
 		stripe.api_key = stripe_secret_key
 		intent = stripe.PaymentIntent.create(
@@ -68,76 +68,118 @@ class CheckoutViewPage(View):
 
 		return render(request, 'checkout/checkout.html', context)
 
-	# END OF COPY
-	# return render(request, 'checkout/checkout.html')
+	def post(self, request):
+		cart = request.session.get('cart', {})
 
+		form_data = {
+			'full_name': request.POST['full_name'],
+			'email': request.POST['email'],
+			'phone_number': request.POST['phone_number'],
+			'postcode': request.POST['postcode'],
+			'town_or_city': request.POST['town_or_city'],
+			'street_address1': request.POST['street_address1'],
+			'street_address2': request.POST['street_address2'],
+			'county': request.POST['county'],
+		}
 
-def post(self, request):
-	bag = request.session.get('bag', {})
-
-	form_data = {
-		'full_name': request.POST['full_name'],
-		'email': request.POST['email'],
-		'phone_number': request.POST['phone_number'],
-		'postcode': request.POST['postcode'],
-		'town_or_city': request.POST['town_or_city'],
-		'street_address1': request.POST['street_address1'],
-		'street_address2': request.POST['street_address2'],
-		'county': request.POST['county'],
-	}
-
-	order_form = OrderForm(form_data)
-	if order_form.is_valid():
-		order = order_form.save(commit=False)
-		pid = request.POST.get('client_secret').split('_secret')[0]
-		order.stripe_pid = pid
-		order.original_bag = json.dumps(bag)
-		order.save()
-		for item_id, item_data in bag.items():
-			try:
-				product = Product.objects.get(id=item_id)
-				if isinstance(item_data, int):
-					order_line_item = OrderLineItem(
-						order=order,
-						product=product,
-						quantity=item_data,
-					)
-					order_line_item.save()
-				else:
-					for size, quantity in item_data['items_by_size'].items():
+		order_form = OrderForm(form_data)
+		if order_form.is_valid():
+			order = order_form.save(commit=False)
+			pid = request.POST.get('client_secret').split('_secret')[0]
+			order.stripe_pid = pid
+			order.original_cart = json.dumps(cart)
+			order.save()
+			for item_id, item_data in cart.items():
+				try:
+					product = Product.objects.get(id=item_id)
+					if isinstance(item_data, int):
 						order_line_item = OrderLineItem(
 							order=order,
 							product=product,
-							quantity=quantity,
-							product_size=size,
+							quantity=item_data,
 						)
 						order_line_item.save()
-			except Product.DoesNotExist:
-				messages.error(request, (
-					"One of the products in your bag wasn't "
-					"found in our database. "
-					"Please call us for assistance!")
-				               )
-				order.delete()
-				return redirect(reverse('view_bag'))
+					else:
+						for size, quantity in item_data['items_by_size'].items():
+							order_line_item = OrderLineItem(
+								order=order,
+								product=product,
+								quantity=quantity,
+								product_size=size,
+							)
+							order_line_item.save()
+				except Product.DoesNotExist:
+					messages.error(request, (
+						"One of the products in your cart wasn't "
+						"found in our database. "
+						"Please call us for assistance!")
+					               )
+					order.delete()
+					return redirect(reverse('view_cart'))
 
-		# Save the info to the user's profile if all is well
-		request.session['save_info'] = 'save-info' in request.POST
-		return redirect(reverse('checkout_success',
-		                        args=[order.order_number]))
+			# Save the info to the user's profile if all is well
+			request.session['save_info'] = 'save-info' in request.POST
+			return redirect(reverse('checkout_success',
+			                        args=[order.order_number]))
 
 
 class CacheCheckoutDataView(View):
-	def get(self, request):
-		return render(request, 'checkout/checkout.html')
-
 	def post(self, request):
-		return render(request, 'checkout/checkout.html')
+		try:
+			pid = request.POST.get('client_secret').split('_secret')[0]
+			stripe.api_key = settings.STRIPE_SECRET_KEY
+			stripe.PaymentIntent.modify(pid, metadata={
+				'cart': json.dumps(request.session.get('cart', {})),
+				'save_info': request.POST.get('save_info'),
+				'username': request.user,
+			})
+			return HttpResponse(status=200)
+		except Exception as e:
+			messages.error(request, ('Sorry, your payment cannot be '
+			                         'processed right now. Please try '
+			                         'again later.'))
+			return HttpResponse(content=e, status=400)
 
 
 class CheckoutSuccessView(View):
-	def get(self, request):
-		return render(request, 'checkout/checkout.html')
+	def get(self, request, order_number):
+		"""
+		   Handle successful checkouts
+		   """
+		save_info = request.session.get('save_info')
+		order = get_object_or_404(Order, order_number=order_number)
 
-	def post(self, request):
-		return render(request, 'checkout/checkout.html')
+		if request.user.is_authenticated:
+			profile = Profile.objects.get(user=request.user)
+			# Attach the user's profile to the order
+			order.user_profile = profile
+			order.save()
+
+			# Save the user's info
+			if save_info:
+				profile_data = {
+					'default_phone_number': order.phone_number,
+					'default_postcode': order.postcode,
+					'default_town_or_city': order.town_or_city,
+					'default_street_address1': order.street_address1,
+					'default_street_address2': order.street_address2,
+					'default_county': order.county,
+				}
+				user_profile_form = ProfileForm(profile_data, instance=profile)
+				if user_profile_form.is_valid():
+					user_profile_form.save()
+
+		messages.success(request, f'Order successfully processed! \
+		       Your order number is {order_number}. A confirmation \
+		       email will be sent to {order.email}.')
+
+		if 'cart' in request.session:
+			del request.session['cart']
+
+		template = 'checkout/checkout_success.html'
+		context = {
+			'order': order,
+		}
+
+		return render(request, template, context)
+
